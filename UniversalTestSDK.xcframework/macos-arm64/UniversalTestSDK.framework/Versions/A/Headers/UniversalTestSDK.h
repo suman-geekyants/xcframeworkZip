57e7d931fedf1a7c6a6de1e64364fa9e9e174aa0
Suman Chatterjee <Suman3.Chatterjee@ril.com>
2025-07-17 17:16:48 +0530

    adding xcframework folder

---
+++ UniversalTestSDK.xcframework/macos-arm64/UniversalTestSDK.framework/Versions/A/Headers/UniversalTestSDK.h
@@ -0,0 +1,18 @@
+//
+//  UniversalTestSDK.h
+//  UniversalTestSDK
+//
+//  Created by Suman Chatterjee on 17/07/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UniversalTestSDK.
+FOUNDATION_EXPORT double UniversalTestSDKVersionNumber;
+
+//! Project version string for UniversalTestSDK.
+FOUNDATION_EXPORT const unsigned char UniversalTestSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UniversalTestSDK/PublicHeader.h>
+
+
